cacc11e57334f30323de4e165d017f7097819e4e
rodney <rjkahane@gmail.com>
2018-07-25 13:51:25 -0400

    working insert

--- hashtable/src/hashtable/App.java
+++ hashtable/src/hashtable/App.java
@@ -4,7 +4,30 @@ public class App {
 
 	public static void main(String[] args) {
 		
+		HashTable table = new HashTable(18);		
+		table.insert("Apple");
+		table.insert("Hello");
+		table.insert("Feeling");
+		table.insert("Water");
+		table.insert("Africa");
+		table.insert("Speed");
+		table.insert("Phone");
+		table.insert("September");
+		table.insert("Michael");
+		table.insert("Milk");
+		table.insert("Huge");
+		table.insert("Dogs");
+		
+		System.out.println("------FIND ELEMENTS------");
+		System.out.println(table.find("Apple"));
+		System.out.println(table.find("Zebra"));
+		System.out.println(table.find("Feeling"));
+		System.out.println(table.find("Water"));
+		System.out.println(table.find("Africa"));
+		System.out.println(table.find("Feeling"));
+		
+		
 
-	}
+	}//end main
 
-}
+}//end class
--- hashtable/src/hashtable/HashTable.java
+++ hashtable/src/hashtable/HashTable.java
@@ -1,14 +1,14 @@
 package hashtable;
 
 public class HashTable {
-	
+
 	String [] hashArray;
 	int arraySize;
 	int size=0;  //counter for number of elements in hash table
 
-	
+
 	public HashTable(int noOfSlots) {
-		
+
 		if(isPrime(noOfSlots)) {
 			hashArray = new String[noOfSlots];
 			arraySize = noOfSlots;
@@ -16,77 +16,80 @@ public class HashTable {
 			int primeCount = getNextPrime(noOfSlots);
 			hashArray = new String[primeCount];
 			arraySize = primeCount;
-			
+
 			System.out.println("Hash table size given "+arraySize+" isnt a prime number");
 			System.out.println("Hash table size changed to: "+primeCount);
 		}
 	}
-	
+
 	private boolean isPrime(int num) {
-		for(int i=2;i*i<=num;i--) {
+		for(int i=2;i*i<=num;i++) {
 			if(num%i==0) {
 				return false;
 			}			
 		}
 		return true;
 	}//end isPrime()
-	
+
 	private int getNextPrime(int minNumber) {
-		
+
 		for(int i=minNumber;true;i++) {
 			if(isPrime(i)) {
 				return i;
 			}
 		}
 	}//end getNextPrime()
-	
+
 	//returns preferred index position
 	private int hashFunc1(String word) {
 		int hashVal = word.hashCode();
 		hashVal = hashVal % arraySize;
-		
+
 		if(hashVal < 0) {
 			hashVal += arraySize;
 		}
 		return hashVal;  //ideal index position we'd like to insert or search in
 	}
-	
+
 	//return step size greater than 0
 	private int hashFunc2(String word) {
 		int hashVal = word.hashCode();
 		hashVal = hashVal % arraySize;
-		
+
 		if(hashVal < 0 ) {
 			hashVal = hashVal + arraySize;
 		}
-		
+
 		return 3-hashVal % 3;
 	}
-	
+
 	public void insert(String word) {
 		int hashVal = hashFunc1(word);
 		int stepSize = hashFunc2(word);
-		
+
 		while(hashArray[hashVal]!=null) {
 			hashVal = hashVal + stepSize;
 			hashVal = hashVal % arraySize;
 		}
-		
+
 		hashArray[hashVal] = word;
 		System.out.println("inserted word: "+word);
 		size++;
 	}
-	
+
 	public String find(String word) {
 		int hashVal = hashFunc1(word);
 		int stepSize = hashFunc2(word);
-		
+
 		while(hashArray[hashVal]!=null) {
+			if(hashArray[hashVal].equals(word)) {
+				return hashArray[hashVal];
+			}
 			hashVal = hashVal + stepSize;
 			hashVal = hashVal % arraySize;
 		}
-		
-		
+		return hashArray[hashVal];
+
 	}
-	
+
 }//end class
